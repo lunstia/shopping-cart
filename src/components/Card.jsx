b7997469d4826022fb925cfd0ee7571ae5ecc6f7
lunstia <136163666+lunstia@users.noreply.github.com>
2023-07-26 07:01:21 -0400

    Basic functionality of site (NO DESIGN)

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import { PropTypes } from 'prop-types'; 
+
+export default function Card({ product }) {
+    console.log('hi')
+    return (
+        <div>
+            <h3>{product.title}</h3>
+            <img src={product.image}/>
+            <p>{product.description}</p>
+            <p>{product.price}</p>
+        </div>
+    );
+}
+
+Card.propTypes = {
+    product: PropTypes.object,
+}
--- src/routes/Jewelry.jsx
+++ src/routes/Jewelry.jsx
@@ -1,18 +1,52 @@
-import {Outlet} from 'react-router-dom'
+import { useEffect, useState } from 'react';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Card from '../components/Card.jsx';
 
 
 function Jewelry() {
+    const [data, setData] = useState();
+    const [error, setError] = useState();
 
-  return (
-    <>
-     <Header />
-     <Outlet />
-     <Footer />
-    </>
-  )
+    useEffect(()=>{
+        const fetchData = async () => {
+            try {
+                const apiData = await fetch('https://fakestoreapi.com/products/category/jewelery');
+                const response = await apiData.json();
+                setData(response);
+            } catch(e) {
+                setError(e);
+            }
+        }
+
+        fetchData();
+    },[]);
+
+    function checkCondition() {
+        if (error) {
+            return 'There seems to be an error, try refreshing';
+        }
+
+        if (!data && !error) {
+            return "Loading"
+        }
+
+        if (data) {
+            return (
+                <div>
+                    {data.map((product) => <Card key={product.id} product={product}/>)}
+                </div>
+            );
+        }
+    }
+    return (
+        <>
+        <Header />
+        {checkCondition()}
+        <Footer />
+        </>
+    )
 }
 
 export default Jewelry
